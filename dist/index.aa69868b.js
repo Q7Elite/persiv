63c1f3903eb2372f5107ecaa4c5958d3b3510ecc
Bonface Gathura <karichubonface@yahoo.com>
2023-06-26 20:41:12 +0300

    remove dist from git ignore.

---
+++ dist/index.aa69868b.js
@@ -0,0 +1,8 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item)=>{
+    item.addEventListener("click", function() {
+        this.classList.toggle("collapsible--expanded");
+    });
+});
+
+//# sourceMappingURL=index.aa69868b.js.map
