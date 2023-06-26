26a7dba5f365c93b04c4a23a3fd22f9e0631d325
Bonface Gathura <karichubonface@yahoo.com>
2023-06-26 19:19:21 +0300

    Add parcel for building

---
+++ public/index.a9f5acef.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".collapsible");e.forEach(e=>{e.addEventListener("click",function(){this.classList.toggle("collapsible--expanded")})});
+//# sourceMappingURL=index.a9f5acef.js.map
